2b6c77de48688337c424a4d9cc50aba1ee358d68
pengzhe <pengzhepost@sina.com>
2018-05-05 17:11:16 +0800

    gRpc

---
+++ src/main/java/com/javaman/grpc/example/Hello.java
@@ -0,0 +1,13 @@
+package com.javaman.grpc.example;
+
+/**
+ * @author pengzhe
+ * @date 2018/5/5 16:47
+ * @description
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("123123");
+    }
+}
